a633dd1727bbeef38db5c1468812d6394131fc05
Jason Robinson <robinsonjason761@gmail.com>
2022-09-13 21:21:46 -0500

    Updating prediction file

--- tf-server/predict.py
+++ tf-server/predict.py
@@ -11,3 +11,21 @@ from tensorflow.keras.datasets.mnist import load_data
 x_test = x_test.reshape((x_test.shape[0], x_test.shape[1], x_test.shape[2], 1))
 # normalize pixel values
 x_test = x_test.astype('float32') / 255.0
+
+url = 'http://localhost:8501/v1/models/img_classifier:predict'
+
+def make_predictions(instances):
+    data = json.dumps({"signature_name": "serving_default", "instances": instances.tolist()})
+    headers = {"content-type": "application/json"}
+    json_response = requests.post(url, data=data, headers=headers)
+    predictions = json.loads(json_response.text)['predictions']
+    return predictions
+
+# get predictions for the first 4 images
+predictions = make_predictions(x_test[:4])
+
+for pred in predictions:
+    print(np.argmax(pred))
+
+for i, pred in enumerate(predictions):
+    print(f"True Value: {y_test[i]}, Predicted Value: {np.argmax(pred)}")
