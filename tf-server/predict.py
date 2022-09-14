d9134923ee52e39ebac266a576f4c4abf15fbf42
Jason Robinson <robinsonjason761@gmail.com>
2022-09-13 20:59:19 -0500

    Updating server

---
+++ tf-server/predict.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import requests
+import json
+import numpy as np
+from tensorflow.keras.datasets.mnist import load_data
+
+#load MNIST dataset
+(_, _), (x_test, y_test) = load_data()
+
+# reshape data to have a single channel
+x_test = x_test.reshape((x_test.shape[0], x_test.shape[1], x_test.shape[2], 1))
+# normalize pixel values
+x_test = x_test.astype('float32') / 255.0
